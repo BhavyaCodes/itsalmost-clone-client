52db71e52578fafc29aec97ef7b7e26e789f61f3
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-18 02:32:11 +0530

    change veriable formData to just data

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,35 +1,34 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 const Landing = () => {
+  const [eventName, setEventName] = useState("");
+  const eventDate = "2020-01-01";
 
-  const [eventName, setEventName] = useState('');
-  const eventDate = '2020-01-01';
-
-  const handleChange = e => {
+  const handleChange = (e) => {
     setEventName(e.target.value);
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const formData = {
+    const data = {
       eventName: eventName,
-      eventDate: eventDate
-    }
+      eventDate: eventDate,
+    };
 
-      axios
-        .post("/api/events", formData)
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err.data));
-    }
+    axios
+      .post("/api/events", data)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err.data));
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <input onChange={handleChange} type="text" value={eventName} />
       <button type="submit"> Create Free Countdown </button>
     </form>
-  )
+  );
 };
 
 export default Landing;
