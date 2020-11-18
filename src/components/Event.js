13ca114e73da0c6fc8d801c3592d859df59994b3
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-18 15:59:14 +0530

    fetch event data with axios

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
 const Event = () => {
   const { id } = useParams();
   console.log(id);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(`/api/events/${id}`);
+      console.log(res.data);
+    };
+    getData();
+  }, [id]);
 
   return <div>Event</div>;
 };
