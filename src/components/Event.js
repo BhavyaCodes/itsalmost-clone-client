3731e6c29b2a87fbe8c4e364502c9dcd57e73a99
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-18 16:42:02 +0530

    display event date and name

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const Event = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
   const { id } = useParams();
   console.log(id);
 
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get(`/api/events/${id}`);
-      console.log(res.data);
+      try {
+        const res = await axios.get(`/api/events/${id}`);
+        setData(res.data);
+      } catch (error) {
+        setError(true);
+      }
     };
     getData();
   }, [id]);
 
-  return <div>Event</div>;
+  return data ? (
+    <div>
+      <h1>{data.eventName}</h1>
+      <h2>{data.eventDate}</h2>
+      <h2>{new Date(data.eventDate).toLocaleString()}</h2>
+      <h2>{new Date(data.eventDate).toString()}</h2>
+    </div>
+  ) : (
+    <div>spinner</div>
+  );
 };
 
 export default Event;
