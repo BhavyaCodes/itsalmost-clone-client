54e78b9fdcc5827ddc5a40766ff5b3ef692a4451
Juggernaut9 <58269749+Juggernaut9@users.noreply.github.com>
2020-11-18 21:33:43 +0530

    display event name and date (#4)
    * scaffold component
    
    * fetch event data with axios
    
    * display event date and name

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./Landing";
+import Event from "./Event";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Landing} />
+          <Route path="/:id" exact component={Event} />
         </Switch>
       </div>
     </BrowserRouter>
