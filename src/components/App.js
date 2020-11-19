f3e112a0c48c1bc29b6f227d3a302ec44b984f0a
Matthew Fee <matthewefee@gmail.com>
2020-11-19 08:37:50 -0600

    fixed pathing so error pages aren't confused with id page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Redirect, BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./Landing";
 import Event from "./Event";
+import Error404 from "./Error404"
+import Error500 from "./Error500"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/:id" exact component={Event} />
+          <Route path="/error/404" exact component={Error404} />
+          <Route path="/error/500" component={Error500} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/Error500.js
@@ -0,0 +1,16 @@
+const Error500 = () => {
+
+    return (
+        <div>
+
+        <h1>500 Error</h1>
+        <div>Oops! Something is not right.</div>
+
+        <a href="./">Create a new Countdown.</a>
+
+        </div>
+      );
+}
+
+
+export default Error500
