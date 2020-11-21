7a9d02125ca25ad5e1ffedc2a253098ffc7fd0f5
Matthew Fee <60991158+lyellgeo@users.noreply.github.com>
2020-11-21 07:59:25 -0600

    Error pages (#8)
    * fixed pathing so error pages aren't confused with id page
    
    * added error paths to App.js
    
    * removed redirect route
    
    * finished error pages and updated links to go back to landing page
    
    * removed path prop so 404 page works correctly
    
    * moved 404 component higher
    
    * Add datetime picker (#7)
    
    * fix routes order in App.js
    
    * update a tags with react-router Link
    
    Co-authored-by: Matthew Fee <matthewefee@gmail.com>
    Co-authored-by: Bryan Urizar <73317691+bryanurizar@users.noreply.github.com>
    Co-authored-by: Juggernau9 <xbhavyatomar@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./Landing";
 import Event from "./Event";
+import Error404 from "./Error404"
+import Error500 from "./Error500"
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Landing} />
+          <Route path="/404" component={Error404} />
+          <Route path="/500" exact component={Error500} />
           <Route path="/:id" exact component={Event} />
         </Switch>
       </div>
---
+++ src/components/Error500.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Error500 = () => {
+
+    return (
+      <div>
+        <h1>500 Error</h1>
+        <div>Oops! Something is not right.</div>
+        <Link to="/">Create a new Countdown.</Link>
+      </div>
+    );
+}
+
+
+export default Error500
