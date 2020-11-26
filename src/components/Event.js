11e1a310b61b9376328fc5bbcddfe368ea9d3f05
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-26 22:17:24 +0530

    temporarily added dates back on Event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import { DateTime } from "luxon";
 
 const Event = () => {
-
   const [data, setData] = useState(null);
   const [error, setError] = useState(false);
   const [timeUntilEvent, setTimeUntilEvent] = useState({
@@ -14,8 +13,8 @@ const Event = () => {
     hours: 0,
     minutes: 0,
     seconds: 0,
-    milliseconds: 0
-  })
+    milliseconds: 0,
+  });
   const { id } = useParams();
 
   useEffect(() => {
@@ -24,11 +23,19 @@ const Event = () => {
         const res = await axios.get(`/api/events/${id}`);
         setData(res.data);
 
-        const  timeLeft = () => {
+        const timeLeft = () => {
           const start = DateTime.local();
           const end = DateTime.fromISO(res.data.eventDate);
-          return end.diff(start, ['years', 'months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds'])
-        }
+          return end.diff(start, [
+            "years",
+            "months",
+            "days",
+            "hours",
+            "minutes",
+            "seconds",
+            "milliseconds",
+          ]);
+        };
         setInterval(() => {
           setTimeUntilEvent(timeLeft());
         }, 1);
@@ -44,12 +51,15 @@ const Event = () => {
   return data ? (
     <div>
       <h1>{data.eventName}</h1>
+      <h2>{data.eventDate}</h2>
+      <h2>{new Date(data.eventDate).toLocaleString()}</h2>
+      <h2>{new Date(data.eventDate).toString()}</h2>
       <div>
         {intervals.map((interval, index) => {
           if (index !== intervals.length - 1) {
-            return <span>{interval} : </span>
+            return <span>{interval} : </span>;
           } else {
-            return <span> {interval}</span>
+            return <span> {interval}</span>;
           }
         })}
       </div>
